09dbc742c71ca86eed84f520792df56c09b86f77
Tiela Black-Law <tiela.rose@gmail.com>
2023-12-13 10:58:26 -0800

    Implement QueueOfStrings

---
+++ week2/QueueOfStrings.java
@@ -0,0 +1,36 @@
+package week2;
+
+public class QueueOfStrings
+{
+    private Node first, last;
+    // private int size = 0;
+    
+    private class Node {
+        String item;
+        Node next;
+    }
+
+    public boolean isEmpty(){
+        return first == null;
+    }
+    public void enqueue(String item){
+        Node oldlast = last;
+        Node last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty())
+            first = last;
+        else 
+            oldlast.next = last;
+        // size++;
+    }
+
+    public String dequeue(){
+        String item = first.item;
+        first = first.next;
+        if (isEmpty())
+            last = null;
+        // size--;
+        return item;
+    }
+}

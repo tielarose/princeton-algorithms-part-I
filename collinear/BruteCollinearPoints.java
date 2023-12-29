82c0cb253f2e905928ed2abcfc766d4d9afdfca3
Tiela Black-Law <tiela.rose@gmail.com>
2023-12-28 20:07:16 -0800

    Handle corner cases in BruteCollinearPoints

---
+++ collinear/BruteCollinearPoints.java
@@ -0,0 +1,38 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import java.util.Arrays;
+
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points) {
+        // corner cases
+        // input is null
+        if (points == null)
+            throw new IllegalArgumentException("Input is null.");
+
+        // contains a null point
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null)
+                throw new IllegalArgumentException("At least one point is null.");
+        }
+
+        // contains a duplicate point
+        Point[] pointsCopy = points.clone();
+        Arrays.sort(pointsCopy);
+        for (int i = 1; i < pointsCopy.length; i++) {
+            if (pointsCopy[i].compareTo(pointsCopy[i - 1]) == 0)
+                throw new IllegalArgumentException("Input contains duplicate points.");
+        }
+    }
+
+    public int numberOfSegments() {
+
+    }
+
+    public LineSegment[] segments() {
+
+    }
+}

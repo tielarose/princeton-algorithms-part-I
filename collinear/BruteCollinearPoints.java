ab0c47fac90123ef84ead25e9f4594f0443ec8d7
Tiela Black-Law <tiela.rose@gmail.com>
2023-12-29 18:57:06 -0800

    Complete BruteCollinearPoints

--- collinear/BruteCollinearPoints.java
+++ collinear/BruteCollinearPoints.java
@@ -4,9 +4,12 @@
  *  Description:
  **************************************************************************** */
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BruteCollinearPoints {
+    private LineSegment[] segments;
+
     public BruteCollinearPoints(Point[] points) {
         // corner cases
         // input is null
@@ -26,13 +29,35 @@ public class BruteCollinearPoints {
             if (pointsCopy[i].compareTo(pointsCopy[i - 1]) == 0)
                 throw new IllegalArgumentException("Input contains duplicate points.");
         }
+
+        // check for collinear points
+        ArrayList<LineSegment> segmentsList = new ArrayList<LineSegment>();
+
+        for (int i = 0; i < pointsCopy.length - 3; i++)
+            for (int j = i + 1; j < pointsCopy.length - 2; j++)
+                for (int k = j + 1; k < pointsCopy.length - 1; k++)
+                    for (int m = k + 1; m < pointsCopy.length; m++) {
+                        double slope1 = pointsCopy[i].slopeTo(pointsCopy[j]);
+                        double slope2 = pointsCopy[i].slopeTo(pointsCopy[k]);
+                        double slope3 = pointsCopy[i].slopeTo(pointsCopy[m]);
+                        if (slope1 == slope2 && slope2 == slope3) {
+                            LineSegment currSegment = new LineSegment(pointsCopy[i], pointsCopy[m]);
+                            if (!segmentsList.contains(currSegment))
+                                segmentsList.add(currSegment);
+                        }
+                    }
+
+        segments = segmentsList.toArray(new LineSegment[segmentsList.size()]);
+
+
     }
 
     public int numberOfSegments() {
+        return segments.length;
 
     }
 
     public LineSegment[] segments() {
-
+        return segments;
     }
 }

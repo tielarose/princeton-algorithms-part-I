a97aafedb2f1eb6daf83666f55f9db8b6848079c
Tiela Black-Law <tiela.rose@gmail.com>
2024-01-07 12:04:50 -0800

    Add Hibbard deletion to BST

--- week4/BST.java
+++ week4/BST.java
@@ -96,10 +96,35 @@ public class BST<Key extends Comparable<Key>, Value> {
             return size(x.left);
     }
 
-    // public void delete(Key key)
-    // {
+    public void delete(Key key)
+    {
+        root = delete(root, key);
+    }
 
-    // }
+    // Hibbard deletion
+    private Node delete(Node x, Key key) {
+        if (x == null)
+            return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0)
+            x.left = delete(x.left, key);
+        else if (cmp > 0)
+            x.right = delete(x.right, key);
+        else 
+        {
+            if (x.right == null)
+                return x.left;
+            if (x.left == null)
+                return x.right;
+
+            Node t = x;
+            x = min(t.right); // we haven't defined this?
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.count = 1 + size(x.left) + size(x.right)
+        return x;
+    }
 
     public void deleteMin() {
         root = deleteMin(root);
